481417252dc7fae2678f2774da1e81ce90f3b037
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-11-20 17:31:00 +0530

    Update color_code_checker.h

--- color_code_checker.h
+++ color_code_checker.h
@@ -1,5 +1,3 @@
 
-#define MAX_COLORPAIR_NAME_CHARS 16
-
 void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor);
 void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber);
